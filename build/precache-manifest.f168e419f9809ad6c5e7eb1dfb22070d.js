472a4f1ea336b8975584cf5b1a63fee201284d6b
GBernardo10 <g_bernardo10@hotmail.com>
2019-12-04 17:21:35 -0300

    heroku deploy

---
+++ build/precache-manifest.f168e419f9809ad6c5e7eb1dfb22070d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45b6aada645e8f2acc47ef32b0b3ecf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3add3159e5fbc42c2580",
+    "url": "/static/css/6.25dc701a.chunk.css"
+  },
+  {
+    "revision": "d89c173c29c7415969f0",
+    "url": "/static/css/7.f1255104.chunk.css"
+  },
+  {
+    "revision": "59c98a43a626e1ea8bb1",
+    "url": "/static/css/8.f1255104.chunk.css"
+  },
+  {
+    "revision": "c21f4007920126219d24",
+    "url": "/static/css/9.f1255104.chunk.css"
+  },
+  {
+    "revision": "beeaa46193240dd91e6d",
+    "url": "/static/css/main.247f66c3.chunk.css"
+  },
+  {
+    "revision": "219c11f46d5a562fc76e",
+    "url": "/static/js/0.68c081cf.chunk.js"
+  },
+  {
+    "revision": "607ecf817dd6b763e809",
+    "url": "/static/js/1.e43eed00.chunk.js"
+  },
+  {
+    "revision": "d3820f41251c35db3557",
+    "url": "/static/js/10.5995cd91.chunk.js"
+  },
+  {
+    "revision": "82e5cf63d13d258b4714",
+    "url": "/static/js/11.e98063b8.chunk.js"
+  },
+  {
+    "revision": "90ac6efc1570bc13a6ef",
+    "url": "/static/js/12.543e28a6.chunk.js"
+  },
+  {
+    "revision": "5acf6493c1751a6a3c8c",
+    "url": "/static/js/4.5dad23d5.chunk.js"
+  },
+  {
+    "revision": "2214221938cf49efd347",
+    "url": "/static/js/5.4a895b29.chunk.js"
+  },
+  {
+    "revision": "3add3159e5fbc42c2580",
+    "url": "/static/js/6.68884080.chunk.js"
+  },
+  {
+    "revision": "d89c173c29c7415969f0",
+    "url": "/static/js/7.16bfde4e.chunk.js"
+  },
+  {
+    "revision": "59c98a43a626e1ea8bb1",
+    "url": "/static/js/8.ad8ced55.chunk.js"
+  },
+  {
+    "revision": "c21f4007920126219d24",
+    "url": "/static/js/9.8d5d4c2a.chunk.js"
+  },
+  {
+    "revision": "beeaa46193240dd91e6d",
+    "url": "/static/js/main.9ec30f7c.chunk.js"
+  },
+  {
+    "revision": "6b368d086f7d8a15647e",
+    "url": "/static/js/runtime-main.94a34ed2.js"
+  },
+  {
+    "revision": "49592611c9cb405accbbd262352c2d4a",
+    "url": "/static/media/3116556.49592611.jpg"
+  },
+  {
+    "revision": "6bba2ac70878ac52ffed3c1eb0123ec9",
+    "url": "/static/media/bg-detalheEventos.6bba2ac7.png"
+  },
+  {
+    "revision": "e2e9a88656fc496ab1d25ce05ec8ee84",
+    "url": "/static/media/bg.e2e9a886.svg"
+  },
+  {
+    "revision": "994cdfa2ac5aa973e80441ce58e4bfe2",
+    "url": "/static/media/fest.994cdfa2.png"
+  },
+  {
+    "revision": "a91c98eb0e985e21e89546ea88cddaa3",
+    "url": "/static/media/user.a91c98eb.png"
+  }
+]);
